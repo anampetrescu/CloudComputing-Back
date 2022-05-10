fd5d94e7c29d54a42babef38a848c4c1a55b059b
anampetrescu <anamaria.petrescu03@gmail.com>
2022-05-10 18:43:43 +0300

    Add first routes

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const messagesRouter = require("./routers/messagesRouter"); //look here
+const bodyParser = require("body-parser");
+
 const app = express();
-app.use(cors())
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/messages', messagesRouter); //and look here too
 
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+    console.log(`Example app listening on port ${port}!`)
 });
